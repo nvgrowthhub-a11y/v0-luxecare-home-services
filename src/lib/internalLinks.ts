672a72ca7bd9ac4c105d9d5f055b529a06613683
nvgrowthhub-a11y <nvgrowthhub@gmail.com>
2026-06-10 15:17:38 +0530

    Create internalLinks.ts

---
+++ src/lib/internalLinks.ts
@@ -0,0 +1,12 @@
+export const internalLinks = {
+  acRepair: [
+    { title: "AC Maintenance Dubai", url: "/ac-maintenance-dubai" },
+    { title: "AC Installation Dubai", url: "/ac-installation-dubai" },
+    { title: "Home Appliance Repair", url: "/home-appliance-repair-dubai" },
+  ],
+
+  washingMachine: [
+    { title: "Dryer Repair Dubai", url: "/dryer-repair-dubai" },
+    { title: "Dishwasher Repair Dubai", url: "/dishwasher-repair-dubai" },
+  ],
+}
