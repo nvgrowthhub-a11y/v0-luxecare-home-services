3609790543070be31b444c7c18bae52cb889dac7
nvgrowthhub-a11y <nvgrowthhub@gmail.com>
2026-06-10 14:38:19 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://nearmeservice.org',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/ac-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/ac-maintenance-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/ac-installation-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/fridge-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/washing-machine-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/dishwasher-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/dryer-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/oven-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/cooking-range-repair-dubai',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://nearmeservice.org/home-appliance-repair-dubai',
+      lastModified: new Date(),
+    },
+  ]
+}
